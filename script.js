e3e24be6f1ecac40fc5616a5f52f8272b8fd8035
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-25 18:30:36 +0200

    fix bug - player can't longer play after game ends yet

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 const GameController = (() => {
-    let _gameMode;
-    let _players = [];
-    let _gameBoard = [
-        0, 0, 0,
-        0, 0, 0,
-        0, 0, 0
-    ];
-
-    let _turn = 1;
-
     const _winningLines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -27,13 +17,24 @@ const GameController = (() => {
         CONTINUE: 3
     }
 
+    let _gameMode;
+    let _players = [];
+    let _gameBoard = [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ];
+
+    let _turn = 1;
+    let _gameEnded = false;
+
     const GameMode = {
         PVP: 1,
         BOT: 2,
         UNKNOWN: 3
     } 
-
-
+    
+    
     function _gameIsOver() {
         for (let i = 0; i < _winningLines.length; ++i) {
             if (_gameBoard[_winningLines[i][0]] === 0) continue;
@@ -41,55 +42,91 @@ const GameController = (() => {
                 _gameBoard[_winningLines[i][1]] === _gameBoard[_winningLines[i][2]]) {
                     GameBoard.animateWinner(_winningLines[i]);
                     return _GameResult.WIN;
-                }
+            }
         }
         if (!_gameBoard.includes(0)) return _GameResult.DRAW;
         return _GameResult.CONTINUE;
+    }
+        
+        
+    function _isTileEmpty(tileNumber) {
+        return gameBoardTiles[tileNumber].textContent === '';
     }
 
     function _toggleTurn() {
         _turn = 3 - _turn;
     }
-
-    function setGameMode(mode) {
-        if (mode === GameMode.PVP | mode === GameMode.BOT) _gameMode = mode;
-        else _gameMode = GameMode.UNKNOWN;
-    }
-
-    function createPlayers(markup) {
-        _players.push(PlayerFactory(markup));
-        _players.push(PlayerFactory(markup === 'X' ? 'O' : 'X'));
-    }
-
-    function makeMove(tileNumber) {
-        const playerMarkup = _players[_turn - 1].markup
-        _gameBoard[tileNumber] = playerMarkup;
-        console.log(_gameBoard[tileNumber]);
-        GameBoard.drawMarkup(tileNumber, playerMarkup);
-        if (_gameIsOver() === _GameResult.WIN) {
-            console.log(`game is over, winner ${playerMarkup}`);
-            return;
-        }
-        if (_gameIsOver() === _GameResult.DRAW) {
-            console.log(`draw`);
-            return;
-        }
-        if (_gameMode === GameMode.BOT) _makeBotMove();
-        if (_gameMode === GameMode.PVP) _toggleTurn();
-    }
-
+        
     function _makeBotMove() {
         let tileNumber;
         do {
             tileNumber = Math.floor(Math.random() * 9);
         } while (_gameBoard[tileNumber] !== 0)
         _gameBoard[tileNumber] = _players[1].markup;
-        GameBoard.drawMarkup(tileNumber, _players[1].markup);
+        GameBoard.drawMarkup(tileNumber, _players[1].markup);        
     }
 
-    function startGame() {
-        _gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    function _updateScore(winner) {
+        switch (winner) {
+            case _players[0]:
+                _players[0].score++;
+                playerOneScoreDisplay.textContent = `Player 1: ${_players[0].score}`;
+                break;
+            case _players[1]:
+                _players[1].score++;
+                playerOneScoreDisplay.textContent = `Player 2: ${_players[1].score}`;
+                break;
+            default:
+               break;
+        }
     }
+        
+    function setGameMode(mode) {
+        if (mode === GameMode.PVP | mode === GameMode.BOT) _gameMode = mode;
+        else _gameMode = GameMode.UNKNOWN;
+    }
+        
+    function createPlayers(markup) {
+        _players.push(PlayerFactory(markup));
+        _players.push(PlayerFactory(markup === 'X' ? 'O' : 'X'));
+    }
+
+    function makeMove(tileNumber) {
+        if (!_isTileEmpty(tileNumber)) {
+            console.log('tile is not empty')
+            return;
+        } 
+
+        const currentPlayer = _players[_turn - 1];
+        
+        if (_gameIsOver() === _GameResult.CONTINUE) {
+            _gameBoard[tileNumber] = currentPlayer.markup;
+            GameBoard.drawMarkup(tileNumber, currentPlayer.markup);    
+        }
+
+        if (_gameIsOver() === _GameResult.WIN) {
+            // disable tile events
+            console.log(`game is over, winner ${currentPlayer.markup}`);
+            if (!gameEdnded) {
+                _updateScore(currentPlayer);
+                _gameEnded = true;
+            }
+            return;
+        }
+
+        if (_gameIsOver() === _GameResult.DRAW) {
+            console.log(`draw`);
+            return;
+        }
+
+        if (_gameMode === GameMode.BOT) _makeBotMove();
+        if (_gameMode === GameMode.PVP) _toggleTurn();
+    }
+
+    
+    // function startGame() {
+    //     _gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    // }
 
     function getGameBoard() {
         return _gameBoard;
@@ -100,7 +137,7 @@ const GameController = (() => {
         setGameMode,
         createPlayers,
         makeMove,
-        startGame,
+        // startGame,
         getGameBoard,
     }
 })();
@@ -150,6 +187,9 @@ const gameDisplay = document.querySelector('section#game-display');
 
 const markupButtons = document.querySelectorAll('div.markup');
 const gameBoardTiles = document.querySelectorAll('.game-board .tile');
+
+const playerOneScoreDisplay = document.querySelector('div.player-score');
+const opponentOneScoreDisplay = document.querySelector('div.opponent-score');
 
 pvpModeButton.addEventListener('click', () => {
     GameController.setGameMode(GameController.GameMode.PVP);
